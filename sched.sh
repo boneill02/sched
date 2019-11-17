918d582e1d231a2ad0a9b26f2948db830ba5fa65
Ben O'Neill <ben@benoneill.xyz>
2019-11-16 20:40:54 -0500

    License into script itself

--- sched.sh
+++ sched.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+# Copyright (C) 2019 Ben O'Neill <ben@benoneill.xyz>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 [ -z "$BLOCKFILE" ] && BLOCKFILE="blocks.txt"
 [ -z "$TIMESFILE" ] && TIMESFILE="times.txt"
@@ -43,10 +57,6 @@ do_sched_blocks() { \
 do_sched_get() { \
 	day="$(date "+%u")"
 	now="$(date "+%H:%M")"
-
-	#debug
-	day="1"
-	now="12:30"
 
 	echo "What schedule?"
 	read SCHEDULE
